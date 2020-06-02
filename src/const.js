f1ea0c3f5a5e29888fdf394f4aaa145f2baa1b03
dongheon lee <niche3600@gmail.com>
2020-06-03 00:29:45 +0900

    add init files

---
+++ src/const.js
@@ -0,0 +1,42 @@
+'use strict'
+
+// userName use for CLI Helper
+exports.userName = ''
+
+/*
+  introduce yourself :)
+  newline use `\n`
+*/
+exports.introduce = ''
+
+/*
+  please write like this example
+  {
+    Label: 'Blog',
+    url: 'https://example.com'
+  },
+  {
+    Label: 'Twitter',
+    url: 'https://twitter.com/user_name'
+  },
+  ...
+*/
+exports.userItems = [
+  {
+    label: '',
+    url: ''
+  },
+  {
+    label: '',
+    url: ''
+  }
+]
+
+// write your icon file name
+exports.imgFileName = 'avatar.png'
+
+// specify your icon size
+exports.imgSize = {
+  width: '80px',
+  height: '80px'
+}
